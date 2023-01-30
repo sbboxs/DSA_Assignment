efb615b6c25bc665c9614cf171ca168a75422253
Chia Jiun Hong <78270716+sbboxs@users.noreply.github.com>
2023-01-31 07:54:19 +0800

    Fixed user login and implemented user home page.

--- DSA_Assignment/DSA_Assignment.cpp
+++ DSA_Assignment/DSA_Assignment.cpp
@@ -1,5 +1,8 @@
-// DSA_Assignment.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//======================================================
+//Student Number :S10222651F, S10223421D
+//Student Name   :Chia Jiun Hong, Syahmi Mirhan Bin Zulkiflee
+//Module  Group  :P02       
+//======================================================
 #include <string>
 #include <iostream>
 #include "User.h"
@@ -9,43 +12,50 @@ using namespace std;
 
 void displayHome();
 void loginForm();
-void loginProcess(Dictionary& userDictionary);
+bool loginProcess(Dictionary& userDictionary, User& currentUser);
 void registerForm();
 void registerProcess(Dictionary& userDictionary);
+void userHome(string userName);
+bool userHomeProcess(Dictionary& userDictionary, User currentUser);
 
 int main()
 {
+	//Defining variables.
 	Dictionary userDictionary;
+	User currentUser;
 	User newUser1("test", "test", false);
 	userDictionary.add(newUser1.getUserName(),newUser1);
 	userDictionary.print();
-
+	bool ifLogin = false;
 	string option = "1";	
 
-	//Login & Registration
+	//Controlling of Login & Registration
 	while (option != "0") {
-		//system("cls");
-		displayHome();
-		cin >> option;
-		//Login
-		if (option == "1") {
-			loginProcess(userDictionary);
-		}
-		else if (option == "2") {
-			registerProcess(userDictionary);
-		}
-		else if (option == "0")
-			cout << "Bye! " << endl;
-
+		if (ifLogin)
+			ifLogin = userHomeProcess(userDictionary, currentUser);
 		else {
-			system("cls");
-			cout << "Sorry. You have entered an invalid option." << endl;
-		}
-	}
-	
-	
-}
+			displayHome();
+			cin >> option;
+			//Login
+			if (option == "1") {
+				ifLogin = loginProcess(userDictionary, currentUser);
+			}
+			else if (option == "2") {
+				registerProcess(userDictionary);
+			}
+			else if (option == "0")
+				cout << "Bye! " << endl;
 
+			else {
+				system("cls");
+				cout << "Sorry. You have entered an invalid option." << endl;
+			}
+		}
+		
+	}
+}
+//=========
+//This function is to print the layout of the homepage. Do not have any input parameters and return value.
 void displayHome() {
 	cout << endl;
 	cout << "Welcome to Ngee Ann Discussion Forum!" << endl;
@@ -55,21 +65,24 @@ void displayHome() {
 	cout << "[0] Quit the application." << endl;
 	cout << "Enter option: ";
 }
-
+//=========
+//This function is to print the layout of login form. Do not have any input parameters and return value.
 void loginForm() {
 	cout << endl;
 	cout << "Login Place!" << endl;
 	cout << "-------------------------" << endl;
-	cout << "[1] Key in login credentials: " << endl;
+	cout << "[1] Key in login credentials. " << endl;
 	cout << "[0] Back to login." << endl;
 	cout << "Enter option: ";
 }
-
-void loginProcess(Dictionary &userDictionary) {
+//=========
+//This function is to control the flow of login form. 
+//The user can perform logging in into their account by inputing the username and password.
+//The user can also perform back to home page.
+bool loginProcess(Dictionary &userDictionary, User &currentUser) {
 	string loginOption = "1";
 	string promptUsername = "";
 	string promptPwd = "";
-	bool loginSuccess = false;
 
 	while (loginOption != "0") {
 		loginForm();
@@ -83,13 +96,13 @@ void loginProcess(Dictionary &userDictionary) {
 			if (promptUsername != "" && promptPwd != "") {
 				User loginUser = userDictionary.get(promptUsername);
 				if (loginUser.getPassword() == promptPwd) {
-					loginSuccess == true;
+					currentUser = loginUser;
 					cout << "Login Success!" << endl;
 					system("pause");
 					system("cls");
+					return true;
 				}
 				else {
-					loginSuccess == false;
 					cout << "Login credentials invalid!" << endl;
 					system("pause");
 				}
@@ -106,8 +119,10 @@ void loginProcess(Dictionary &userDictionary) {
 			cout << "Sorry. You have entered an invalid option." << endl;
 		}
 	}
+	return false;
 }
-
+//=========
+//This function print the layout of the register form. Do not have any input parameters and return value.
 void registerForm() {
 	cout << endl;
 	cout << "Registration Place!" << endl;
@@ -116,7 +131,10 @@ void registerForm() {
 	cout << "[0] Back to login." << endl;
 	cout << "Enter option: ";
 }
-
+//=========
+//This function control the flow of register form.
+//The user can perform registering account by inputing the "Unique" username, password and confirm his password.
+//The user can also perform back to home page.
 void registerProcess(Dictionary& userDictionary) {
 	string registerOption = "1";
 	string newUsername;
@@ -166,6 +184,41 @@ void registerProcess(Dictionary& userDictionary) {
 		}
 	}
 }
-void forumMenu() {
+void userHome(string userName) {
 	cout << endl;
+	cout << "Welcome back! Dear user: " << userName << endl;
+	cout << "---------------------------" << endl;
+	cout << "[1] View all topics and posts " << endl;
+	cout << "[2] View my posts" << endl;
+	cout << "[3] Create new posts" << endl;
+	cout << "[0] Logout" << endl;
+}
+
+bool userHomeProcess(Dictionary& userDictionary, User currentUser) {
+	string userHomeOption = "1";
+	string userName = currentUser.getUserName();
+	while (userHomeOption != "0") {
+		userHome(userName);
+		cin >> userHomeOption;
+		if (userHomeOption == "1"){
+			cout << "All topics and posts" << endl;
+		}
+		else if (userHomeOption == "2") {
+			cout << "My posts" << endl;
+		}
+		else if (userHomeOption == "3") {
+			cout << "Create new posts" << endl;
+		}
+		else if (userHomeOption == "0") {
+			system("cls");
+			cout << "Has logged out. ";
+			return false;
+		}
+		else {
+			system("cls");
+			cout << "Sorry. You have entered an invalid option." << endl;
+		}
+
+	}
+	return true;
 }
